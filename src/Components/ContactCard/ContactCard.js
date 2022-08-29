190bc88bb21ba74968e7b24a47147f9c00298873
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-08-29 19:00:55 +0300

    data update

--- src/Components/ContactCard/ContactCard.js
+++ src/Components/ContactCard/ContactCard.js
@@ -1,18 +1,19 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 import styles from './ContactCard.style';
 const ContactList = ({data}) => {
-  console.log(data.photo);
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
       <View>
-        <Image style={styles.profilePhoto} source={{uri: `${data.photo}`}} />
+        <Image style={styles.profilePhoto} source={{uri: data.photo}} />
       </View>
       <View>
-        <Text style={styles.name}>{data.firstName}</Text>
+        <Text style={styles.name}>
+          {data.firstName} {data.lastName}
+        </Text>
         <Text style={styles.lastSeen}>Çevrimiçi</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- src/Screens/Main/Contact/Contact.js
+++ src/Screens/Main/Contact/Contact.js
@@ -1,4 +1,4 @@
-import {View, Text, FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
 import React from 'react';
 import Header from '../../../Components/Header/Header';
 import ContactCard from '../../../Components/ContactCard/ContactCard';
@@ -8,6 +8,7 @@ const Contact = () => {
     <View>
       <Header />
       <FlatList
+        style={{height: '85%'}}
         data={ContactData}
         renderItem={({item}) => <ContactCard data={item} />}
       />
